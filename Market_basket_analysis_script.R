9fb954a7cf6a41b0e55b9ba1f6a4089335fa07de
saimore027 <saimore027@gmail.com>
2020-05-12 02:27:16 +0530

    New Commit for MBA

--- Market_basket_analysis_script.R
+++ Market_basket_analysis_script.R
@@ -4,6 +4,7 @@ groceries <- read.csv("D:\\mywork\\R project\\Market_basket_analysis_3rd_yr\\gro
 rules <- apriori(groceries)
 rules <- apriori(groceries, parameter = list(supp = 0.0001, conf = 0.8))
 inspect(rules[1:5])
+summary(rules)
 rules<-sort(rules, by="support", decreasing=T)
 inspect(rules[1:5])
 rules
@@ -16,5 +17,6 @@ inspect(rules[1:10])
 rules_margarine <- apriori(groceries,parameter = list(supp = 0.001, conf = 0.8), appearance = list(default = "rhs", lhs = "margarine="))
 rules_margarine <- apriori(groceries,parameter = list(supp = 0.00000001, conf = 0.1), appearance = list(default = "rhs", lhs = "citrus.fruit=canned beer"))
 inspect(rules_margarine[1:2])
-plot(rules, method = "graph")
+top20 <- rules[1:20]
+plot(top20, method = "graph")
 plot(rules, method = "graph", interactive = T)
